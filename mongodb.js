c20622b349e58beb4e0db1651ea16a0871dae345
csmith120 <taskmaster3654@gmail.com>
2025-02-05 13:50:44 -0600

    find method (vid 9)

--- mongodb.js
+++ mongodb.js
@@ -14,21 +14,24 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) =>
 
     const db = client.db(databaseName)
 
-    db.collection('users').findOne({ _id: new ObjectID("67a3b1507ba2ee501c8b539a") }, (error, user) => {
-        if (error) {
-            return console.log('couldnt find user!')
-        }
+    //db.collection('users').findOne({ _id: new ObjectID("67a3b1507ba2ee501c8b539a") }, (error, user) => {
+    //    if (error) {
+    //        return console.log('couldnt find user!')
+    //    }
+//
+    //    console.log(user)
+    //})
 
-        console.log(user)
+    //db.collection('users').find({ age: 27 }).toArray((error, users) =>{
+    //    console.log(users)
+    //})
+
+    db.collection('tasks').findOne({ _id: new ObjectID("67a273ce1a9cd985a0c4efd6") }, (error, task) => {
+        console.log(task)
     })
 
-    db.collection('users').find({ age: 27 }).toArray((error, users) =>{
-        if (error) {
-            return console.log('couldnt find users!')
-        }
-
-        console.log(users)
+    db.collection('tasks').find({ completed: false }).toArray ((error, tasks) => {
+        console.log(tasks)
     })
-
 
 })
