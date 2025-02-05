a9e4a615ea9b8add44a454f91efad4af75426ff5
csmith120 <taskmaster3654@gmail.com>
2025-02-05 12:39:57 -0600

    learned about the id (vid 8)

--- mongodb.js
+++ mongodb.js
@@ -1,10 +1,13 @@
 // Crud create read update delete
 
-const mongodb = require('mongodb')
-const MongoClient = mongodb.MongoClient
+const { MongoClient, ObjectID } = require('mongodb')
 
 const connectionURL = 'mongodb://127.0.0.1:27017'
 const databaseName = 'task-manager'
+
+const id = new ObjectID()
+console.log(id.id.length)
+console.log(id.toHexString().length)
 
 MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) => {
     if (error) {
@@ -14,13 +17,13 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) =>
     const db = client.db(databaseName)
 
     //db.collection('users').insertOne({
-    //    name: 'task',
-    //    age: '18'
+    //    name: 'mike',
+    //    age: '19'
     //}, (error, result) => {
     //    if (error) {
     //        return console.log('Unable to insert user')
     //    }
-
+//
     //    console.log(result.ops)
     //})
 
@@ -40,24 +43,25 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) =>
     //    console.log(result.ops)
     //})
 
-    db.collection('tasks').insertMany([
-        {
-            description: 'lemon code',
-            completed: true
-        },
-        {
-            description: 'lime code',
-            completed: true
-        },
-        {
-            description: 'gpa code',
-            completed: false
-        },
-    ], (error, result) => {
-        if (error) {
-            return console.log('unable to insert documents')
-        }
+    //db.collection('tasks').insertMany([
+    //    {
+    //        description: 'lemon code',
+    //        completed: true
+    //    },
+    //    {
+    //        description: 'lime code',
+    //        completed: true
+    //    },
+    //    {
+    //        description: 'gpa code',
+    //        completed: false
+    //    },
+    //], (error, result) => {
+    //    if (error) {
+    //        return console.log('unable to insert documents')
+    //    }
+    //    console.log(result.ops)
+    //})
 
-        console.log(result.ops)
-    })
+
 })
