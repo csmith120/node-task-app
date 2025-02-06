35e4bc69e6d2712c874319dc6cac874a68bf6ea4
csmith120 <taskmaster3654@gmail.com>
2025-02-06 13:13:07 -0600

    learned delete (vid 12)

--- mongodb.js
+++ mongodb.js
@@ -15,24 +15,16 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) =>
     const db = client.db(databaseName)
 
 
-    //db.collection('users').updateOne({
-    //    _id: new ObjectID("67a500a33cac0e83d4ec3ed7")
-    //}, {
-    //    $inc: {
-    //        age: 1
-    //    }
+    //db.collection('users').deleteMany({
+    //    age: 27
     //}).then((result) => {
     //    console.log(result)
     //}).catch((error) => {
     //    console.log(error)
     //})
 
-    db.collection('tasks').updateMany({
-        completed: false
-    }, {
-        $set: {
-            completed: true
-        }
+    db.collection('tasks').deleteOne({
+        description: "lime code"
     }).then((result) => {
         console.log(result)
     }).catch((error) => {
