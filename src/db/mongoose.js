5306fcb248a98725c8cd28545018afc5cd600498
csmith120 <taskmaster3654@gmail.com>
2025-02-10 13:11:45 -0600

    name and age requirments (7:09)

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -5,25 +5,30 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     useCreateIndex: true
 })
 
-//const User = mongoose.model('User', {
-//    name: {
-//        type: String
-//    },
-//    age: {
-//        type: Number
-//    }
-//})
+const User = mongoose.model('User', {
+    name: {
+        type: String,
+        required: true
+    },
+    age: {
+        type: Number,
+        validate(value) {
+            if (value < 0) {
+                throw new Error('age must be a positive number')
+            }
+        }
+    }
+})
 
-//const me = new User({
-//    name: 'Christian',
-//    age: 18
-//})
-//
-//me.save().then(() =>{
-//console.log(me)
-//}).catch((error) => {
-//    console.log('Error!' ,error)
-//})
+const me = new User({
+    name: 'Mike'
+})
+
+me.save().then(() =>{
+console.log(me)
+}).catch((error) => {
+    console.log('Error!' ,error)
+})
 
 const Tasks = mongoose.model('Tasks', {
     desription:{ 
@@ -34,13 +39,13 @@ const Tasks = mongoose.model('Tasks', {
     }
 })
 
-const task = new Tasks({
-    desription: "plant",
-    compeleted: true
-})
-
-task.save().then(() => {
-    console.log(task)
-}).catch((error) => {
-    console.log('Error!', error)
-})
+//const task = new Tasks({
+//    desription: "plant",
+//    compeleted: true
+//})
+//
+//task.save().then(() => {
+//    console.log(task)
+//}).catch((error) => {
+//    console.log('Error!', error)
+//})
