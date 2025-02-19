0fb2a9b6c5a5a3182d772c5fda81d1d5a9407431
csmith120 <taskmaster3654@gmail.com>
2025-02-18 22:15:34 -0600

    added module folder and moved user code and removed some useless code

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,62 +1,9 @@
 const mongoose = require('mongoose')
-const validator = require('validator')
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     useNewUrlParser: true,
     useCreateIndex: true
 })
-
-const User = mongoose.model('User', {
-    name: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    email: {
-        type: String,
-        required: true,
-        trim: true,
-        lowercase: true,
-        validate(value) {
-            if (!validator.isEmail(value)) {
-                throw new Error('Email is invalid')
-            }
-        }
-    },
-    password: {
-        type: String,
-        required: true,
-        trim: true,
-        minlength: 7,
-        validate(value) {
-            if (value.toLowerCase().includes("password")) {
-                throw new Error('password cannot contain password (You are not clever)')
-            }
-        }
-    },
-    age: {
-        type: Number,
-        default: 0,
-        validate(value) {
-            if (value < 0) {
-                throw new Error('age must be a positive number')
-            }
-        }
-    }
-})
-
-//const me = new User({
-//    name: 'Jim',
-//    email: 'TESTEMAIL@Work.com',
-//    password: 'LOLFUNNY38',
-//    age: 23
-//})
-//
-//me.save().then(() =>{
-//console.log(me)
-//}).catch((error) => {
-//    console.log('Error!' ,error)
-//})
 
 const Tasks = mongoose.model('Tasks', {
     desription:{ 
@@ -68,14 +15,4 @@ const Tasks = mongoose.model('Tasks', {
         type: Boolean,
         default: false
     }
-})
-
-const task = new Tasks({
-    desription: 'to kill a deer'
-})
-
-task.save().then(() => {
-    console.log(task)
-}).catch((error) => {
-    console.log('Error!', error)
 })
--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 const express = require('express')
+require('./db/mongoose')
+const User = require('./model/user')
 
 const app = express()
 const port = process.env.PORT || 3000
 
+app.use(express.json())
+
 app.post('/users', (req, res) => {
-    res.send('teseting')
+    const user = new User(req.body)
+
+    user.save().then(() => {
+        res.send(user)
+    }).catch((e) => {
+        res.status(400)
+        res.send(e)
+    })
 })
 
 app.listen(port, () => {
