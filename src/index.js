18c7ea12c77be34915046c45ee74ee27ebb83ac4
csmith120 <taskmaster3654@gmail.com>
2025-02-18 20:40:07 -0600

    8 (6:58)

--- src/index.js
+++ src/index.js
@@ -2,3 +2,11 @@ const express = require('express')
 
 const app = express()
 const port = process.env.PORT || 3000
+
+app.post('/users', (req, res) => {
+    res.send('teseting')
+})
+
+app.listen(port, () => {
+    console.log('server is up on port ' + port)
+})
