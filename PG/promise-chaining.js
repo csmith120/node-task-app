381025f3fb377c1e59757bc1298f1f0570f93b1c
csmith120 <taskmaster3654@gmail.com>
2025-02-24 14:02:35 -0600

    vid 11_13

--- PG/promise-chaining.js
+++ PG/promise-chaining.js
@@ -1,2 +1,11 @@
 require('../src/db/mongoose')
 const User = require('../src/model/user')
+
+User.findByIdAndUpdate('67b5590c6816719288c4e52b', { age: 1}).then((user) => {
+    console.log(user)
+    return User.countDocuments({ age: 1 })
+}).then((result) => {
+    console.log(result)
+}).catch((e) => {
+    console.log(e)
+})
