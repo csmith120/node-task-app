fb14c474d5aba489d8563cf87ba545d6e59f175c
csmith120 <taskmaster3654@gmail.com>
2025-03-03 13:24:43 -0600

    fixed the update bug

--- src/routers/user.js
+++ src/routers/user.js
@@ -59,7 +59,7 @@ router.patch('/users/:id', async (req, res) => {
     try {
         const user = await User.findById(req.params.id)
         updates.forEach((update) => user[update] = req.body[update])
-        await user.save
+        await user.save()
 
         if (!user) {
             return res.status(404).send()
