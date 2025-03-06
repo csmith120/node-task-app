c0d68aa0615c73a73e8acfa093e8809b0cd84c7a
csmith120 <taskmaster3654@gmail.com>
2025-03-06 12:46:49 -0600

    added a logout and logout All (12_11)

--- src/routers/user.js
+++ src/routers/user.js
@@ -29,6 +29,29 @@ router.get('/users/me', auth, async (req, res) => {
     res.send(req.user)
 })
 
+router.post('/users/logout', auth, async (req, res) => {
+    try {
+        req.user.tokens = req.user.tokens.filter((token) => {
+            return token.token !== req.token
+        })
+        await req.user.save()
+
+        res.send()
+    } catch (e) {
+        res.status(500).send()
+    }
+})
+
+router.post('/users/logoutAll', auth, async (req, res) => {
+    try {
+        req.user.tokens = []
+        await req.user.save()
+        res.send()
+    } catch (e) {
+        res.status(500).send()
+    }
+})
+
 router.get('/users/:id', async (req,res) => {
     const _id = req.params.id
 
